17a684a2851f0d8aa2747b956e6c9fa1ea6c5510
Tom Parker <palfrey@tevp.net>
2011-05-29 19:52:27 +0100

    Initial version of test code

---
+++ test.py
@@ -0,0 +1,22 @@
+from os import listdir
+from os.path import join
+from generate_next import generate_next
+
+test_dir = "test"
+
+tests = {}
+
+for line in open(join(test_dir, "list.txt")):
+	(name, to, fname) = line.strip().split(",")
+	tests[fname] = (name, to)
+
+for fname in listdir(test_dir):
+	if fname.find(".xml")!=-1:
+		found = False
+		for (name, to) in generate_next(join(test_dir, fname), lambda x:False):
+			assert not found
+			assert fname in tests, (fname, (name, to))
+			assert tests[fname] == (name, to), (tests[fname], (name, to))
+			found = True
+		assert found, fname
+
