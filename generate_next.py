15b82fdb22dc9b3d451fa70af745a1a9dd968f81
Tom Parker <palfrey@tevp.net>
2011-05-30 22:14:24 +0100

    Assume 2-letter namespaces are cross-wiki language links

--- generate_next.py
+++ generate_next.py
@@ -163,6 +163,8 @@ def generate_next(fname, existing):
 						continue
 					if namespace in ("file", "image", "template", "wikipedia", "wikt", "category", "wp", "wikinvest", "wiktionary"):
 						continue
+					if len(namespace) == 2:
+						continue # assume language link
 					if newlink.find("UTC")==-1 and newlink.find(": ")==-1:
 						raise Exception, (current, newlink, earlierText[:l.end()])
 
