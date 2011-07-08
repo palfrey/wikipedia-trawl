a924daab55125e498bbe6024ea898de164d1c4e2
Tom Parker <palfrey@tevp.net>
2011-07-08 21:35:17 +0100

    Add lots more tests

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from os import listdir
 from os.path import join
 import generate_next
 import codecs
+from sys import stderr
 
 generate_next.debug = True
 
@@ -15,13 +16,15 @@ for line in codecs.open(join(test_dir, "list.txt"), "rb", "utf-8"):
 
 for fname in listdir(test_dir):
 	if fname.find(".xml")!=-1:
+		print >>stderr, fname
 		found = False
 		for (name, to, redirect) in generate_next.generate_next(join(test_dir, fname), lambda x,y:False):
-			if redirect:
-				continue
+			if to == None:
+				to = "None"
 			assert not found
 			assert fname in tests, ("%s,%s,%s"%(name, to, fname))
 			assert tests[fname] == (name, to), (tests[fname], (name, to))
+			print "good match", name, to
 			found = True
 		assert found, fname
 
